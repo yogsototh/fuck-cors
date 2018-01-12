5d1303ac45cc9bf6fbeab65939f10d0a19a1ebd1
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2018-01-12 11:01:12 +0100

    updated dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject fuck-cors "0.1.6"
+(defproject fuck-cors "0.1.7"
   :description "Fuck CORS and open all to everyone"
   :url "http://github.com/yogsototh/fuck-cors"
   :license {:name "MIT"
             :url "http://opensource.org/licences/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]])
