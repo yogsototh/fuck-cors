fefe219294ebdc1bc26f735e239b7c3e2db8ee19
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2024-08-26 02:41:01 +0200

    update url to private git forge

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject fuck-cors "0.1.8"
   :description "Fuck CORS and open your API to everyone"
-  :url "http://github.com/yogsototh/fuck-cors"
+  :url "https://git.esy.fun/yogsototh/fuck-cors"
   :license {:name "MIT"
             :url "http://opensource.org/licences/MIT"}
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo" :creds :gpg}]
+                        ["snapshots" {:url "https://clojars.org/repo" :creds :gpg}]]
   :dependencies [[org.clojure/clojure "1.11.4"]])
